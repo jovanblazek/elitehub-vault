ca7025cd924b1e57d7c705cd472013d511622aad
Jovan Blažek <jovan.blazek.22@gmail.com>
2026-04-09 22:42:53 +0200

    Remove system deletion

--- apps/eddn-worker/src/mq/queues/eddn/events/docked.ts
+++ apps/eddn-worker/src/mq/queues/eddn/events/docked.ts
@@ -1,11 +1,15 @@
 import type { EDDNJournalDockedMessage } from '@elitehub/eddn-contracts'
 import { db } from '../../../../db/db.js'
-import { buildPartialSystemData } from '../helpers/systemHelpers.js'
+import { buildPartialSystemData, shouldUpsertSystemFromDocked } from '../helpers/systemHelpers.js'
 import { upsertStationFromDocked } from '../helpers/stationHelpers.js'
 import { upsertSystem } from '../helpers/systemHelpers.js'
 
 export const processDockedEvent = async (message: EDDNJournalDockedMessage) => {
   await db.transaction(async (tx) => {
+    if (!shouldUpsertSystemFromDocked(message)) {
+      return
+    }
+
     const system = await upsertSystem(tx, buildPartialSystemData(message))
     await upsertStationFromDocked(tx, message, system.id)
   })
