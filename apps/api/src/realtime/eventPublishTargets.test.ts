79ec497ab1770944a479ec6f7713a90e2f681a54
Jovan Blažek <36740941+jovanblazek@users.noreply.github.com>
2026-05-09 21:58:31 +0200

    chore: Housekeeping (#24)
    * Update logs
    
    * Fix SENTRY_RELEASE set incorrectly
    
    * Migrate to vitest
    
    * Bump ts
    
    * Update readme
    
    * Update docs

--- apps/api/src/realtime/eventPublishTargets.test.ts
+++ apps/api/src/realtime/eventPublishTargets.test.ts
@@ -1,5 +1,5 @@
 import assert from 'node:assert/strict'
-import test from 'node:test'
+import { test } from 'vitest'
 import { buildPublishTargetsForOutboxRow } from './eventPublishTargets.js'
 
 test('buildPublishTargetsForOutboxRow builds faction publish targets', () => {
